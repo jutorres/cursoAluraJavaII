bc17150ce53a85d434e278b8fcacd43a6cee0068
jutorres <torresjujuliana@gmail.com>
2017-11-16 20:30:23 -0300

    Class 5 completed

--- src/br/com/caelum/conta/Main.java
+++ src/br/com/caelum/conta/Main.java
@@ -3,19 +3,11 @@ package br.com.caelum.conta;
 public class Main {
 
 	public static void main(String[] args) {
-		Conta cc = new ContaCorrente();
-		Conta cp = new ContaPoupanca();
-		
-        cc.deposita(1000); 
-        cp.deposita(1000);
 
-		AtualizadorDeContas adc = new AtualizadorDeContas(0.01);
+		ContaCorrente cc = new ContaCorrente();
+		cc.deposita(1000.567);
 
-        adc.roda(cc);
-        adc.roda(cp);
-
-        System.out.println("Saldo Total: " + adc.getSaldoTotal());
-		
+		System.out.printf("O saldo é: %.2f", cc.getSaldo());
 	}
-	
+
 }
